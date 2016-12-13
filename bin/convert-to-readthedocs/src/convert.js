471fcb0a389e4af58a97affbb1c3e9a0a3cb73c1
Boy Baukema <boy@ibuildings.nl>
2016-12-13 13:04:24 +0100

    Reverse WIP commit

--- bin/convert-to-readthedocs/src/convert.js
+++ bin/convert-to-readthedocs/src/convert.js
@@ -12,10 +12,7 @@ let asvs = require('owasp-asvs'),
     pdc = require('pdc'),
     aasvs = require('./../../../src/aasvs.json'),
     includes = require('array-includes'),
-    lang = 'en',
-    chapterNumber = 4,
-    h2Number = 0,
-    requirementChapter = 0;
+    lang = 'en';
 
 /* when Array#includes is not present */
 delete Array.prototype.includes;
@@ -32,26 +29,115 @@ asvs.requirements.forEach(function(requirement) {
         return;
     }
 
-    if (!requirement.levels.includes(2)) {
-        return;
-    }
-
-    if (requirementChapter != requirement.chapterNr) {
-        chapterNumber++;
-        h2Number = 0;
-        requirementChapter = requirement.chapterNr;
-
-        console.log('# ' + chapterNumber + '. ' + asvs.chapters['' + requirementChapter]['name'][lang] + "\n");
-    }
-
-    h2Number++;
-
-    let title = chapterNumber + '.' + h2Number + '. V' + requirement.chapterNr + '.' + requirement.nr + ': ' + annotatedRequirement.shortTitle[lang];
+    let title = requirement.chapterNr + '.' + requirement.nr + ' ' + annotatedRequirement.shortTitle[lang];
 
     let requirementDoc = `${title}
-${"-".repeat(title.length)}
-### ${requirement.title[lang]}
+${"=".repeat(title.length)}
+
+${requirement.title[lang]}
+
+Levels: ${requirement.levels.join(", ")}
+
 `;
 
-    console.log(requirementDoc);
+    let helpFilesDir = `${__dirname}/../../../src/help/${requirement.chapterNr}/${requirement.nr}/${lang}/`;
+    fs.readdir(helpFilesDir, function (err, files) {
+        if (err) {
+            files = [];
+        }
+        Promise.all(files.map(function(file) {
+            return new Promise(function(resolve, reject) {
+                fs.readFile(helpFilesDir + file, function(err, result) {
+                    if (err) {
+                        reject(err);
+                    }
+
+                    let helpTextInMdFormat = `${aasvs.helpTypes[file][lang]}
+${"-".repeat(file.length)}
+
+${result}`;
+
+                    resolve(helpTextInMdFormat);
+                });
+            });
+        })).then(function(helpTextsInMdFormat) {
+            return Promise.all(helpTextsInMdFormat.map(function(helpTextInMdFormat) {
+                return new Promise(function(resolve, reject) {
+                    pdc(helpTextInMdFormat, 'markdown', 'rst', function(err, result) {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve(result);
+                    });
+                })
+            }));
+        }).then(function(helpTextsInRstFormat) {
+            requirementDoc += helpTextsInRstFormat.join("\n\n");
+
+            let requirementFilePath = DOC_DIR + 'requirement-' + requirement.chapterNr + '.' + requirement.nr + '.rst';
+            fs.writeFile(requirementFilePath, requirementDoc, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                console.log("Wrote" + requirementFilePath);
+            });
+        });
+    });
 });
+
+
+// build chapter indexes
+for (let chapterNr in asvs.chapters) {
+    let chapterName = asvs.chapters[chapterNr].name[lang];
+
+    let fileNames = asvs.requirements.reduce(function(fileNames, requirement) {
+        if (requirement.chapterNr !== chapterNr) {
+            return fileNames;
+        }
+        fileNames[fileNames.length] = 'requirement-' + chapterNr + '.' + requirement.nr;
+        return fileNames;
+    }, []);
+
+    let chapterDoc = `v${chapterNr} ${chapterName}
+=${"=".repeat(chapterNr.length)}=${"=".repeat(chapterName.length)}
+
+.. toctree::
+  :maxdepth: 1
+  :titlesonly:
+
+  ${fileNames.join("\n  ")}
+`;
+
+    let chapterFilePath = DOC_DIR + 'v' + chapterNr + '.rst';
+    fs.writeFile(chapterFilePath, chapterDoc);
+    console.log('Wrote ' + chapterFilePath);
+}
+
+
+// build level indexes
+for (let levelNr in asvs.levelNames) {
+    let levelName = asvs.levelNames[levelNr][lang];
+
+    let fileNames = asvs.requirements.reduce(function(fileNames, requirement) {
+        if (!requirement.levels.includes(+levelNr)) {
+            return fileNames;
+        }
+        fileNames[fileNames.length] = 'requirement-' + requirement.chapterNr + '.' + requirement.nr;
+        return fileNames;
+    }, []);
+
+    let levelDoc = `Level ${levelNr}: ${levelName}
+======${"=".repeat(levelNr.length)}==${"=".repeat(levelName.length)}
+
+.. toctree::
+  :maxdepth: 1
+  :titlesonly:
+
+  ${fileNames.join("\n  ")}
+`;
+
+    let levelFilePath = DOC_DIR + 'level' + levelNr + '.rst';
+    fs.writeFile(levelFilePath, levelDoc);
+    console.log('Wrote ' + levelFilePath);
+}
